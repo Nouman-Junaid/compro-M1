6dab1314789ad3e051eafc0c8f250947bd1fec4f
Nouman-Junaid <noumanjunaid.sce@gmail.com>
2024-06-10 10:30:06 -0700

    position add

--- script.js
+++ script.js
@@ -14,8 +14,23 @@ if (close) {
     })
 }
 @media (max-width: 477px){
+    .section-p1 {
+        padding:20px ;
+
+    }
     #header {
         padding: 10px 30px;
+
+    }
+    h1 {
+        font-size: 38px:
+    }
+    h2 {
+        font-size: 32px;
+    }
+    #hero {
+      padding:0 20px;
+      background-position: 55%;
 
     }
 }
